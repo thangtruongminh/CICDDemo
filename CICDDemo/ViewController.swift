ec94dd92a176804b68f6bea4ff089b4f01a3bf8c
Truong Minh Thang <thangtruong.dev@gmail.com>
2021-07-09 09:30:58 +0700

    crash

--- CICDDemo/ViewController.swift
+++ CICDDemo/ViewController.swift
@@ -9,10 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var butto: UIButton!
+    var x : Int!
+    @IBOutlet weak var butto: UIButto!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        var y = x
     }
 
 

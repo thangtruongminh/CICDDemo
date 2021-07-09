1f858d2d875d3f801e52b211758d2800f39987a8
Truong Minh Thang <thangtruong.dev@gmail.com>
2021-07-09 07:19:40 +0700

    change name of outlet

--- CICDDemo/ViewController.swift
+++ CICDDemo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var button: UIButton!
+    @IBOutlet weak var butto: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
